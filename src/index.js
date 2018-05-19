091612f8877890f5bf129bd2444863baf6279e70
Abdulaziz <alobodi1@hotmail.com>
2018-05-19 05:27:31 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import * as ui from "./ui";
 
 fetch(`http://localhost:5000/yoururl-297a3/us-central1/api/`)
   .then(payload => payload.json())
-  .then(({ message }) => (ui.getElement("app").innerHTML = `${message}`));
+  .then(({ message }) => (ui.getElement("app").innerHTML = `<h1 class="text-danger">${message}</h1>`));
