7530f423aa6cb1adcebf873a5439695461e71069
Abdulaziz <alobodi1@hotmail.com>
2018-05-19 04:56:18 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as ui from "./ui";
 
-const message = fetch(``)
+fetch(`http://localhost:5000/yoururl-297a3/us-central1/api/`)
   .then(payload => payload.json())
-  .then(message => message);
-ui.getElement("app").innerHTML = `${message}`;
+  .then(({ message }) => (ui.getElement("app").innerHTML = `${message}`));
